e046296a28bb474983eccbb93188dcaf52c6a7b9
Petya Choeva <petya.choeva@gmail.com>
2024-01-21 22:19:05 +0000

    Styled header and nagivation bar

--- site/src/jsMain/kotlin/com/pcapps/personalwebsite/components/Header.kt
+++ site/src/jsMain/kotlin/com/pcapps/personalwebsite/components/Header.kt
@@ -3,37 +3,61 @@ package com.pcapps.personalwebsite.components
 import androidx.compose.runtime.Composable
 import com.pcapps.personalwebsite.models.Section
 import com.pcapps.personalwebsite.models.Theme
+import com.pcapps.personalwebsite.styles.LogoStyle
+import com.pcapps.personalwebsite.styles.NavigationItemStyle
 import com.pcapps.personalwebsite.util.Constants.FONT_FAMILY
 import com.pcapps.personalwebsite.util.Res
 import com.varabyte.kobweb.compose.css.FontWeight
+import com.varabyte.kobweb.compose.css.TextDecorationLine
 import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
 import com.varabyte.kobweb.compose.ui.modifiers.*
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.icons.fa.FaBars
+import com.varabyte.kobweb.silk.components.icons.fa.IconSize
 import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
+import org.jetbrains.compose.web.css.px
+
 import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
 
 @Composable
 fun Header() {
+    val breakpoint = rememberBreakpoint()
     Row(
         modifier = Modifier
-            .fillMaxWidth(80.percent)
+            .fillMaxWidth(if(breakpoint > Breakpoint.MD) 80.percent else 90.percent)
             .margin(topBottom = 50.px),
         horizontalArrangement = Arrangement.SpaceBetween,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        LeftSide()
-        RightSide()
+        LeftSide(breakpoint = breakpoint)
+
+        //resolution is bigger than a tablet
+        if(breakpoint > Breakpoint.MD) {
+            RightSide()
+        }
     }
 }
 
 @Composable
-fun LeftSide() {
-    Row {
+fun LeftSide(breakpoint: Breakpoint) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        if(breakpoint <= Breakpoint.MD) {
+            FaBars(
+                modifier = Modifier.margin(right = 15.px),
+                size = IconSize.XL
+            )
+        }
+
         Image(
+            modifier = LogoStyle.toModifier(),
             src = Res.Image.logo,
             description = "Logo Image"
         )
@@ -52,11 +76,12 @@ fun RightSide() {
     ) {
         Section.values().take(6).forEach {section ->
             Link(
-                modifier = Modifier
+                modifier = NavigationItemStyle.toModifier()
                     .padding(right = 30.px)
                     .fontFamily(FONT_FAMILY)
                     .fontSize(18.px)
-                    .fontWeight(FontWeight.Normal),
+                    .fontWeight(FontWeight.Normal)
+                    .textDecorationLine(TextDecorationLine.None),
                 path = section.path,
                 text = section.title
             )
