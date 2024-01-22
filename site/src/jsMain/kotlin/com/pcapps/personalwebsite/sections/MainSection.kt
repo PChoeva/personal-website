2c8c939afa70d5e981dbf269033a5d4bb9aaf820
Petya Choeva <petya.choeva@gmail.com>
2024-01-22 00:04:24 +0000

    Added Main content & animations

--- site/src/jsMain/kotlin/com/pcapps/personalwebsite/sections/MainSection.kt
+++ site/src/jsMain/kotlin/com/pcapps/personalwebsite/sections/MainSection.kt
@@ -4,19 +4,40 @@ import androidx.compose.runtime.Composable
 import com.pcapps.personalwebsite.components.Header
 import com.pcapps.personalwebsite.components.SocialBar
 import com.pcapps.personalwebsite.models.Section
+import com.pcapps.personalwebsite.models.Theme
+import com.pcapps.personalwebsite.styles.MainButtonStyle
+import com.pcapps.personalwebsite.styles.MainImageStyle
+import com.pcapps.personalwebsite.util.Constants.FONT_FAMILY
 import com.pcapps.personalwebsite.util.Constants.SECTION_WIDTH
+import com.pcapps.personalwebsite.util.Constants.SUMMARY_TEXT
 import com.pcapps.personalwebsite.util.Res
-import com.varabyte.kobweb.compose.css.ObjectFit
+import com.varabyte.kobweb.compose.css.*
+import com.varabyte.kobweb.compose.foundation.layout.Arrangement
 import com.varabyte.kobweb.compose.foundation.layout.Box
 import com.varabyte.kobweb.compose.foundation.layout.Column
+import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.*
+import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.graphics.Image
+import com.varabyte.kobweb.silk.components.layout.SimpleGrid
+import com.varabyte.kobweb.silk.components.layout.numColumns
+import com.varabyte.kobweb.silk.components.navigation.Link
+import com.varabyte.kobweb.silk.components.style.breakpoint.Breakpoint
+import com.varabyte.kobweb.silk.components.style.toModifier
+import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
+import org.jetbrains.compose.web.css.percent
 import org.jetbrains.compose.web.css.px
+import org.jetbrains.compose.web.dom.Button
+import org.jetbrains.compose.web.dom.Col
+import org.jetbrains.compose.web.dom.P
+import org.jetbrains.compose.web.dom.Text
 
 @Composable
 fun MainSection() {
+    val breakpoint = rememberBreakpoint()
     Box (
         modifier = Modifier
             .maxWidth(SECTION_WIDTH.px)
@@ -24,7 +45,7 @@ fun MainSection() {
         contentAlignment = Alignment.TopCenter
     ) {
         MainBackground()
-        MainContent()
+        MainContent(breakpoint = breakpoint)
     }
 }
 
@@ -40,12 +61,124 @@ fun MainBackground() {
 }
 
 @Composable
-fun MainContent() {
+fun MainContent(breakpoint: Breakpoint) {
     Column (
         modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Header()
-        SocialBar()
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Bottom,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            SimpleGrid(
+                modifier = Modifier.fillMaxWidth(
+                    if(breakpoint >= Breakpoint.MD) 80.percent
+                    else 90.percent
+                ),
+                numColumns = numColumns(base = 1, md = 2)
+            ) {
+                MainText(breakpoint = breakpoint)
+                MainImage()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainText(breakpoint: Breakpoint) {
+    Row(
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        if(breakpoint > Breakpoint.MD) {
+            SocialBar()
+        }
+        Column {
+            P(
+                attrs = Modifier
+                    .margin(topBottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(if(breakpoint >= Breakpoint.LG) 45.px else 20.px)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Primary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Hello, I am")
+            }
+
+            P(
+                attrs = Modifier
+                    .margin(top = 20.px, bottom = 0.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(if(breakpoint >= Breakpoint.LG) 68.px else 40.px)
+                    .fontWeight(FontWeight.Bolder)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Petya Choeva")
+            }
+
+            P(
+                attrs = Modifier
+                    .margin(top = 10.px, bottom = 5.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(20.px)
+                    .fontWeight(FontWeight.Bold)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text("Mobile & Web Developer / Designer")
+            }
+
+            P(
+                attrs = Modifier
+                    .margin(bottom = 25.px)
+                    .maxWidth(400.px)
+                    .fontFamily(FONT_FAMILY)
+                    .fontSize(15.px)
+                    .fontStyle(FontStyle.Italic)
+                    .fontWeight(FontWeight.Normal)
+                    .color(Theme.Secondary.rgb)
+                    .toAttrs()
+            ) {
+                Text(SUMMARY_TEXT)
+            }
+
+            Button(
+                attrs = MainButtonStyle.toModifier()
+                    .height(40.px)
+                    .border(width = 0.px)
+                    .borderRadius(r = 5.px)
+                    .backgroundColor(Theme.Primary.rgb)
+                    .color(Colors.White)
+                    .cursor(Cursor.Pointer)
+                    .toAttrs()
+            ) {
+                Link(
+                    modifier = Modifier
+                        .color(Colors.White)
+                        .textDecorationLine(TextDecorationLine.None),
+                    text = "Hire me",
+                    path = Section.Contact.path
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun MainImage() {
+    Column(
+        modifier = Modifier.fillMaxSize(80.percent).fillMaxHeight(),
+        verticalArrangement = Arrangement.Bottom
+    ) {
+        Image(
+            modifier = MainImageStyle.toModifier().fillMaxWidth(),
+            src = Res.Image.main,
+            description = "Main Image"
+        )
     }
 }
--- site/src/jsMain/kotlin/com/pcapps/personalwebsite/util/Constants.kt
+++ site/src/jsMain/kotlin/com/pcapps/personalwebsite/util/Constants.kt
@@ -5,11 +5,17 @@ object Constants {
     const val GITHUB = "https://github.com/PChoeva"
     const val SECTION_WIDTH = 1920
     const val FONT_FAMILY = "Roboto"
+//    const val FONT_FAMILY = "Ariel"
+
+    const val SUMMARY_TEXT = "Results-driven Application Designer/Software Engineer with a strong background in Web / Mobile Applications Development. Demonstrated leadership in delivering critical projects like Repository for Middlesex University. Proficient in Application /\n" +
+            "Solution Design using JavaScript, Java, Kotlin, applying strategic thinking and problem-solving. Extensive experience in working as part of a team, applying good\n" +
+            "communication and planning skills. I am a quick learner and self-starter. Seeking impactful roles to apply diverse expertise and deliver innovative tech solutions."
 }
 
 object Res {
     object Image {
         const val background = "background.svg"
         const val logo = "logo.svg"
+        const val main = "main_image.png"
     }
 }
